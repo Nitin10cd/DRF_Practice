b9247088e51008eb6f51dd4bcecb754fb59673b7
Nitin10cd <nsaxenacse@gmail.com>
2026-01-09 03:11:18 +0530

    serializer basic example

--- DrjangoRestFramework/api/views.py
+++ DrjangoRestFramework/api/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from students.models import Students
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
 def studentViews(request):
     students = Students.objects.all()
-    #serializing the data
-    students_list = list(students.values())
-    return JsonResponse(students_list,safe=False)
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
